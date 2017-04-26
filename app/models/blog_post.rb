fcbd5037246acdcaa1782e809dfd9863d78e5230
mauriciomartin <mauriciomartin@me.com>
2017-04-26 16:03:14 -0400

    first commit

---
+++ app/models/blog_post.rb
@@ -0,0 +1,6 @@
+class BlogPost < ApplicationRecord
+	validates :title, :blog_entry, presence: true
+	validates :title, uniqueness: true, length: {maximum: 140}
+
+	has_many :comments
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+ 
+
+  devise_for :users
+  resources :comments, only: [:edit, :create, :update, :destroy]
+  resources :blog_posts
+
+  root 'blog_posts#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
